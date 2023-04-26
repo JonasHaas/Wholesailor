6d667b94392a3dc6fb55ae9496306c7579162e2d
JonasHaas <haas.qt@gmail.com>
2023-04-26 17:31:50 +0200

    add some extra function to get logs insight

--- dev_environment_watch.sh
+++ dev_environment_watch.sh
@@ -8,6 +8,13 @@ cleanup() {
   exit 0
 }
 
+# Function to follow the logs of the second project's containers
+follow_logs() {
+  echo "Following logs for the wholesailor project's containers:"
+  docker-compose logs -f &
+  logs_pid=$!
+}
+
 # Trap SIGINT and EXIT signals
 trap cleanup SIGINT EXIT
 
@@ -19,26 +26,17 @@ echo "Starting first Docker Compose project (woocommerce-example-shop)..."
 echo "Starting second Docker Compose project (wholesailor)..."
 (docker-compose up -d --build)
 
-# Function to display logs of the second project's containers
-#show_logs() {
-#  echo "Displaying logs for the wholesailor project's containers:"
-#  (docker-compose logs)
-#}
-
-# Wait for x seconds before showing the logs
-#sleep 10
-
-# Display logs for the wholesailor project's containers
-#show_logs
-
-# application endpoint
-echo "Access the application at: http://127.0.0.1:5000/products"
-echo ""
+# Follow the logs of the second project's containers
+follow_logs
 
 # Watch for changes in the app's source code
-echo "Watching for changes in the app's source code..."
-while true; do
-  inotifywait -r -e modify,create,delete ./backend/app
+while inotifywait -r -e modify,attrib,close_write,move,create,delete ./backend; do
   echo "Changes detected. Rebuilding the app container..."
   (docker-compose up -d --build)
+
+  # Kill the previous logs process
+  kill $logs_pid
+
+  # Follow the logs again
+  follow_logs
 done
