be884c368bc058e28ab297b6a276f0441efd9336
JonasHaas <haas.qt@gmail.com>
2023-05-15 21:56:51 +0200

    wip terraform

--- app/backend/src/__init__.py
+++ app/backend/src/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
-# app = Flask(__name__, static_url_path="/../../frontend/index.html")
+# app = Flask(__name__)
+app = Flask(__name__, static_url_path="/../../frontend/index.html")
 CORS(app)
 
 from . import main
---
+++ terraform/resources/dynamodb.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "products" {
+  name         = "products"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "name"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Wholesailor-DynamoDB-Table"
+    Environment = "prod"
+  }
+}
