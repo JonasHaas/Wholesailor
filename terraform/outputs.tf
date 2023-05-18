c3bd15acea75b4a4a9565952ad452413dd78414d
JonasHaas <haas.qt@gmail.com>
2023-05-18 07:51:47 +0200

    add ecr config

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "website_url" {
   description = "The URL of the website hosted in the S3 bucket"
   value       = "https://${aws_s3_bucket.frontend_bucket.bucket}.s3.${data.aws_region.current.name}.amazonaws.com/index.html"
 }
+
+output "uri" {
+  value       = aws_ecr_repository.ecr_repository.repository_url
+  description = "The URI of the ECR repository."
+}
