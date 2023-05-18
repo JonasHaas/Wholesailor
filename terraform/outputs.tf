79c1c096383a1f1cdaa8c65f4d6994d666d99907
JonasHaas <haas.qt@gmail.com>
2023-05-18 07:09:30 +0200

    fix url

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "website_url" {
   description = "The URL of the website hosted in the S3 bucket"
-  value       = "https://${aws_s3_bucket.frontend_bucket.bucket}.s3-website-${data.aws_region.current.name}.amazonaws.com"
+  value       = "https://${aws_s3_bucket.frontend_bucket.bucket}.s3.${data.aws_region.current.name}.amazonaws.com/index.html"
 }
