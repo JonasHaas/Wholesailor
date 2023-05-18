fff29e9c0d7930ca58dd853e6643948ff72564a5
JonasHaas <haas.qt@gmail.com>
2023-05-18 07:05:35 +0200

    remove force_destroy

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,21 +1,18 @@
-# resource "aws_dynamodb_table" "products" {
-#   name         = "products"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "id"
-#   range_key    = "name"
+resource "aws_dynamodb_table" "products" {
+  name         = "products"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "name"
 
-#   attribute {
-#     name = "id"
-#     type = "N"
-#   }
+  attribute {
+    name = "id"
+    type = "N"
+  }
 
-#   attribute {
-#     name = "name"
-#     type = "S"
-#   }
+  attribute {
+    name = "name"
+    type = "S"
+  }
 
-#   tags = {
-#     Name        = "Wholesailor-DynamoDB-Table"
-#     Environment = "prod"
-#   }
-# }
+  tags = var.common_tags
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,8 @@
 
 resource "aws_s3_bucket" "frontend_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
-  tags          = var.common_tags
+  bucket = var.bucket_name
+  #force_destroy = true
+  tags = var.common_tags
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
