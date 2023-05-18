6ba708175015fa0608e1546ed13dda369a394850
JonasHaas <haas.qt@gmail.com>
2023-05-18 10:07:21 +0200

    remove handler and add tags

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,18 +14,20 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+
+  tags = var.common_tags
 }
 
 resource "aws_lambda_function" "process_products" {
   function_name = "process-products"
 
   role      = aws_iam_role.iam_for_lambda.arn
-  image_uri = aws_ecr_repository.ecr_repository.repository_url
+  image_uri = "${aws_ecr_repository.ecr_repository.repository_url}:latest"
 
   package_type = "Image"
   timeout      = 60
 
-  handler = "app.handler"
-
   depends_on = [aws_ecr_repository.ecr_repository]
+
+  tags = var.common_tags
 }
